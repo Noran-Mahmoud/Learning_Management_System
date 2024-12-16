5757b67e598e4fb406a6b093720f48f8323a6c66
esraamahmoudd <147391506+esraamahmoudd@users.noreply.github.com>
2024-12-17 00:05:00 +0200

    Add files via upload

---
+++ learning_managment_system/src/main/java/com/learning_managment_system/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.learning_managment_system.repository;
+import java.util.List;
+import com.learning_managment_system.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByInstructorId(Long instructorId);
+
+}
+
