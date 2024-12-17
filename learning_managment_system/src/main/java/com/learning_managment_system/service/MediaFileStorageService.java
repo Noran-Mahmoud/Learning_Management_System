e38a658c93c29c2523421010d255ac0a0b38d513
esraamahmoudd <147391506+esraamahmoudd@users.noreply.github.com>
2024-12-17 02:25:17 +0200

    Update MediaFileStorageService.java

--- learning_managment_system/src/main/java/com/learning_managment_system/service/MediaFileStorageService.java
+++ learning_managment_system/src/main/java/com/learning_managment_system/service/MediaFileStorageService.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Service
 public class MediaFileStorageService {
 
-    private final String storageLocation = "media_files"; // مجلد تخزين الملفات
+    private final String storageLocation = "media_files"; 
 
     public MediaFileStorageService() throws IOException {
         Path storagePath = Paths.get(storageLocation);
@@ -22,25 +22,25 @@ public class MediaFileStorageService {
         }
     }
 
-    // ميثود لتحميل ملف الوسائط
+
     public String uploadFile(MultipartFile file) throws IOException {
-        // التحقق من أن الملف ليس فارغًا
+    
         if (file.isEmpty()) {
             throw new IllegalArgumentException("File is empty");
         }
 
-        // إنشاء اسم فريد للملف لتجنب التكرار
+        
         String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
         Path filePath = Paths.get(storageLocation, fileName);
 
-        // حفظ الملف في المسار المحدد
+    
         Files.copy(file.getInputStream(), filePath);
 
-        // إعادة مسار الملف لحفظه في قاعدة البيانات
+        
         return filePath.toString();
     }
 
-    // ميثود لاسترجاع ملف من المسار
+    
     public byte[] getFile(String filePath) throws IOException {
         Path path = Paths.get(filePath);
         if (!Files.exists(path)) {
@@ -49,7 +49,7 @@ public class MediaFileStorageService {
         return Files.readAllBytes(path);
     }
 
-    // ميثود لحذف ملف
+    
     public void deleteFile(String filePath) throws IOException {
         Path path = Paths.get(filePath);
         Files.deleteIfExists(path);
