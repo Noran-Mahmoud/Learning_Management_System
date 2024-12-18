85458b054f77526443f6ef6d9ec00ff4825656ea
YASMEENELSHAMY <147400556+YASMEENELSHAMY@users.noreply.github.com>
2024-12-18 06:43:33 +0200

    Update UserController.java

--- learning_managment_system/src/main/java/com/learning_managment_system/controller/UserController.java
+++ learning_managment_system/src/main/java/com/learning_managment_system/controller/UserController.java
@@ -3,6 +3,7 @@ package com.learning_managment_system.controller;
 import com.learning_managment_system.model.User;
 import com.learning_managment_system.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +17,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    // Register a new user with role-based access
+    // Admin Only Can Create Users
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/register")
     public ResponseEntity<User> registerUser(@RequestBody User user) {
         User savedUser = userService.registerUser(user);
@@ -29,29 +31,31 @@ public class UserController {
         String response = userService.loginUser(username, password);
         return ResponseEntity.ok(response);
     }
-
-    // Get user by username
-    @GetMapping("/{username}")
-    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
-        User user = userService.getUserByUsername(username);
-        return ResponseEntity.ok(user);
-    }
-
     // Get all users (Admin only)
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/all")
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
     }
-
-    // Update user profile
+    // The account holder or admin only
+   @PreAuthorize("hasRole('ADMIN') or principal.username == #username")
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        User user = userService.getUserByUsername(username);
+        return ResponseEntity.ok(user);
+    }
+    // The account holder or admin only
+    @PreAuthorize("hasRole('ADMIN') or principal.username == #username")
     @PutMapping("/{username}/profile")
     public ResponseEntity<User> updateUserProfile(@PathVariable String username, @RequestBody User updatedUser) {
         User user = userService.updateUserProfile(username, updatedUser);
         return ResponseEntity.ok(user);
     }
 
+
     // Delete a user (Admin only)
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{username}")
     public ResponseEntity<String> deleteUser(@PathVariable String username) {
         userService.deleteUser(username);
