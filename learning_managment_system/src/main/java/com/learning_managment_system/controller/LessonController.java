ad07050bbcdf2ee645c2ceabf1dd450655666b1a
esraamahmoudd <147391506+esraamahmoudd@users.noreply.github.com>
2024-12-20 13:31:20 +0200

    Update LessonController.java

--- learning_managment_system/src/main/java/com/learning_managment_system/controller/LessonController.java
+++ learning_managment_system/src/main/java/com/learning_managment_system/controller/LessonController.java
@@ -1,7 +1,5 @@
 package com.learning_managment_system.controller;
 
-
-
 import com.learning_managment_system.service.AttendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +15,18 @@ public class LessonController {
 
     @PreAuthorize("hasRole('INSTRUCTOR')")
     @PostMapping("/{lessonId}/generate-otp")
-    public ResponseEntity<String> generateOtp(@PathVariable Long lessonId) {
-        String otp = attendanceService.generateOtpForLesson(lessonId);
+    public ResponseEntity<String> generateOtp(@PathVariable Long lessonId, @RequestParam Long studentId) {
+        String otp = attendanceService.generateOtpForLesson(lessonId, studentId);
         return ResponseEntity.ok(otp);
     }
 
     @PreAuthorize("hasRole('STUDENT')")
     @PostMapping("/{lessonId}/validate-otp")
-    public ResponseEntity<Boolean> validateOtp(@PathVariable Long lessonId, @RequestParam String otp) {
-        boolean isValid = attendanceService.validateOtp(lessonId, otp);
+    public ResponseEntity<Boolean> validateOtp(
+            @PathVariable Long lessonId,
+            @RequestParam Long studentId,
+            @RequestParam String otp) {
+        boolean isValid = attendanceService.validateOtp(lessonId, studentId, otp);
         return ResponseEntity.ok(isValid);
     }
 }
