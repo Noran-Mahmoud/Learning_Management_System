269529104c0aae80bb61a42dbcdfd7ef7de8036f
esraamahmoudd <147391506+esraamahmoudd@users.noreply.github.com>
2024-12-20 03:16:29 +0200

    Add files via upload

---
+++ learning_managment_system/src/main/java/com/learning_managment_system/repository/LessonAttendanceRepository.java
@@ -0,0 +1,15 @@
+package com.learning_managment_system.repository;
+
+import com.learning_managment_system.model.LessonAttendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LessonAttendanceRepository extends JpaRepository<LessonAttendance, Long> {
+
+    Optional<LessonAttendance> findByLessonIdAndOtp(Long lessonId, String otp);
+
+
+}
