39a7b9ac341c41be13dce257deb1a38919453741
Noran Mahmoud <noran.m.ismail05@gmail.com>
2024-12-23 05:06:48 +0200

    Update Course.java add FetchType.EAGER

--- learning_managment_system_version0.2/src/main/java/com/learning_managment_system/model/Course.java
+++ learning_managment_system_version0.2/src/main/java/com/learning_managment_system/model/Course.java
@@ -34,12 +34,12 @@ public class Course {
     @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private Set<Lesson> lessons;
-
+    
     @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private Set<Assessment> assessments;
 
-    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     @JsonManagedReference("course-question")
     private Set<Question> questionBank;
 
@@ -51,97 +51,4 @@ public class Course {
         this.mediaFileUrl = mediaFileUrl;
     }
     public Course(){}
-
-    public Course(String title, Long id, String description, String duration, String mediaFileUrl, User instructor, List<User> enrolledStudents, Set<Lesson> lessons, Set<Assessment> assessments, Set<Question> questionBank) {
-        this.title = title;
-        this.id = id;
-        this.description = description;
-        this.duration = duration;
-        this.mediaFileUrl = mediaFileUrl;
-        this.instructor = instructor;
-        this.enrolledStudents = enrolledStudents;
-        this.lessons = lessons;
-        this.assessments = assessments;
-        this.questionBank = questionBank;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDuration() {
-        return duration;
-    }
-
-    public void setDuration(String duration) {
-        this.duration = duration;
-    }
-
-    public String getMediaFileUrl() {
-        return mediaFileUrl;
-    }
-
-    public void setMediaFileUrl(String mediaFileUrl) {
-        this.mediaFileUrl = mediaFileUrl;
-    }
-
-    public @NotNull User getInstructor() {
-        return instructor;
-    }
-
-    public void setInstructor(@NotNull User instructor) {
-        this.instructor = instructor;
-    }
-
-    public List<User> getEnrolledStudents() {
-        return enrolledStudents;
-    }
-
-    public void setEnrolledStudents(List<User> enrolledStudents) {
-        this.enrolledStudents = enrolledStudents;
-    }
-
-    public Set<Lesson> getLessons() {
-        return lessons;
-    }
-
-    public void setLessons(Set<Lesson> lessons) {
-        this.lessons = lessons;
-    }
-
-    public Set<Assessment> getAssessments() {
-        return assessments;
-    }
-
-    public void setAssessments(Set<Assessment> assessments) {
-        this.assessments = assessments;
-    }
-
-    public Set<Question> getQuestionBank() {
-        return questionBank;
-    }
-
-    public void setQuestionBank(Set<Question> questionBank) {
-        this.questionBank = questionBank;
-    }
 }
